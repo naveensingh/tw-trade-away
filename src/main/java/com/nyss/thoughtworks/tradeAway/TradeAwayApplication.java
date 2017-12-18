69c04550d547f1667a938e8047c3662f2514d76e
Kumar Yashwant <kr.yashwant@outlook.com>
2017-12-18 12:40:15 +0530

    #002 Yash/Sumit Project Structure/ Initial Project Structure Check In

---
+++ src/main/java/com/nyss/thoughtworks/tradeAway/TradeAwayApplication.java
@@ -0,0 +1,12 @@
+package com.nyss.thoughtworks.tradeAway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeAwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeAwayApplication.class, args);
+	}
+}
